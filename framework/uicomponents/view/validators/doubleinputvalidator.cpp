e833c023e55cb6523e83f52b3bce2112f96aa909
Gabriel Sartori <gabriel@embarc.com.br>
2025-01-02 10:31:16 +0100

    Avoid crash when validate empty string

--- framework/uicomponents/view/validators/doubleinputvalidator.cpp
+++ framework/uicomponents/view/validators/doubleinputvalidator.cpp
@@ -53,6 +53,11 @@ void DoubleInputValidator::fixup(QString& string) const
         }
     };
 
+    if (string.isEmpty()) {
+        string.append("0");
+        return;
+    }
+
     if (string.startsWith(".")) {
         string.prepend("0");
     }
