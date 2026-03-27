eb7676036ff843d7dc92d843b7f16af43877a588
Dmitry Makarenko <kryksyh@gmail.com>
2026-03-27 12:53:44 +0300

    update 260327_2

--- framework/global/thirdparty/kors_modularity/modularity/modulesioc.h
+++ framework/global/thirdparty/kors_modularity/modularity/modulesioc.h
@@ -33,7 +33,8 @@ SOFTWARE.
 
 #include "imoduleinterface.h"
 
-#define IOC_CHECK_INTERFACE_TYPE
+// TODO: re-enable in next update
+#define __IOC_CHECK_INTERFACE_TYPE
 
 namespace kors::modularity {
 template<class I>
